b123f26e9632e903d918ac188524a3e55fb51de9
Frederik Hegger <frederik.hegger@inf.h-brs.de>
2012-07-02 13:30:13 +0200

    add state for base placement

--- raw_scenarios/raw_generic_states/src/generic_navigation_states.py
+++ raw_scenarios/raw_generic_states/src/generic_navigation_states.py
@@ -4,6 +4,9 @@ roslib.load_manifest('raw_generic_states')
 import rospy
 import smach
 import smach_ros
+import actionlib
+import raw_base_placement_to_platform_in_front.msg
+import raw_srvs.srv
 
 from simple_script_server import *
 sss = simple_script_server()
@@ -35,5 +38,32 @@ class approach_pose(smach.State):
                 print 'last state: ',base_state
                 return "failed"
             
+
+class adjust_pose_wrt_platform(smach.State):
+
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['succeeded', 'failed'])
+
+    def execute(self, userdata):
+        ac_base_adj = actionlib.SimpleActionClient('/scan_front_orientation', raw_base_placement_to_platform_in_front.msg.OrientToBaseAction)
             
+        rospy.loginfo("Waiting for action server <</scan_front_orientation>> to start ...");
+        ac_base_adj.wait_for_server()
+        rospy.loginfo("action server <</scan_front_orientation>> is ready ...");
+        action_goal = raw_base_placement_to_platform_in_front.msg.OrientToBaseActionGoal()
+            
+        action_goal.goal.distance = 0.02;
+        rospy.loginfo("send action");
+        ac_base_adj.send_goal(action_goal.goal);
+        
+        rospy.loginfo("wait for base to adjust");
+        finished_before_timeout = ac_base_adj.wait_for_result()
+    
+        if finished_before_timeout:
+            rospy.loginfo("Action finished: %s", ac_base_adj.get_state())
+            return 'succeeded'    
+        else:
+            rospy.logerr("Action did not finish before the time out!")
+            return 'failed'
+        
             
